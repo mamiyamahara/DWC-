d06776a199e85077641a176c63d619f688d33a56
mamiyamahara <y.mami0324@gmail.com>
2020-08-07 12:41:56 +0900

    [Finish] 2-8

--- Ruby/第八章/practice.rb
+++ Ruby/第八章/practice.rb
@@ -1,35 +1,8 @@
-apple = 110
+puts "プーさん可愛い"
+input_key = gets
+puts "入力された内容は"+input_key
 
-if apple >= 100
-	puts "りんごの値段は100円以上です" #true=>処理される
-end
-
-if apple <= 100
-	puts "りんごの値段は100円以下です" #false=>処理されない
-end
-
-
-tall = 180
-if tall >= 170 && tall <= 190
-	puts "身長は170以上190以下です。"
-end
-
-
-apple = "Aomori"
-
-if apple =="Nagano" #もしappleがNaganoなら
-	puts "りんごの生産地は長野です"
-else
-	puts "りんごの生産地は青森です"
-end
-
-
-apple = "Yamagata"
-
-if apple == "Nagano"      #もしappleがNaganoなら
-  puts "りんごの生産地は長野です"
-elsif apple == "Yamagata" #それともappleがYamagataなら
-  puts "りんごの生産地は山形です"
-else                      #どちらも当てはまらない場合は
-  puts "りんごの生産地は青森です"
-end
+puts "キーボードで数字「2」と数字「3」を入力してください"
+a=gets.to_i
+b=gets.to_i
+puts "a+b=#{a+b}"
